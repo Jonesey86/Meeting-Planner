4bf3f71af52e53a794f4741ba56fc26b1f8a402a
Jonesey86 <irj082024@gmail.com>
2024-12-03 12:48:03 +0100

    meeting-planner initial commit

---
+++ model.cs
@@ -0,0 +1,11 @@
+using Meetingplanner.Controller;
+
+namespace Meetingplanner.Models;
+public class Meeting
+{
+    public string? Title { get; set; }
+    public string? Day {get; set;}
+    public List<string>? Participants { get; set; } 
+    public TimeOnly StartTime { get; set; } 
+    public TimeOnly EndTime { get; set; }
+}
